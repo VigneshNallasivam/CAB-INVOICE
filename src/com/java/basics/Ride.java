f02a8150ee92468994e9cc300cb3419e0cdf9fda
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-07 20:43:33 +0530

    Added : UC-1 Calculating Fare For Multiple Rides

---
+++ src/com/java/basics/Ride.java
@@ -0,0 +1,13 @@
+package com.java.basics;
+
+public class Ride
+{
+	public final int time;
+	public double distance;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
